bc2c7aa10000e1e93cae39727706c47a94418f89
CHristian <Vasquezimanol07@gmail.com>
2025-05-02 23:28:43 -0400

    Primer commit en C-proyect

--- obj/Debug/net8.0/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77be0214bd9d652b34fc4d353e0363bf2a804dba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e43dbb341ceefa93d4e6e324fa85d2769a56809")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
